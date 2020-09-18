e87d79f821a5f9a412db25b737e447dc5fb17b51
Tanjila Shamima <58725488+TanjilaShamima@users.noreply.github.com>
2020-09-18 18:01:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
         function doSomething() {
 
-            fetch('https://www.boredapi.com/api/activity/')
+            fetch('http://www.boredapi.com/api/activity/')
             .then(response => response.json())
             .then(data => {
                 
